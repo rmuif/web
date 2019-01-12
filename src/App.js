18373445ad797173aec846e31173ee8c33d0633e
Linus Långberg <langberglinus@gmail.com>
2019-01-12 23:39:11 +0100

    Add Firebase initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
+import firebase from 'firebase/app';
+
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+
+// Initialize Firebase
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
+
+firebase.initializeApp(config);
 
 const title = 'React + Material-UI + Firebase';
 
