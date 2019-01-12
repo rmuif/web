c8cee2261b69e11084f59abd8691b77fa19098ce
Linus Långberg <langberglinus@gmail.com>
2019-01-12 23:32:48 +0100

    Add bar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const title = 'React + Material-UI + Firebase';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppBar color="primary" position="static">
+        <Toolbar variant="regular">
+          <Typography style={{ flexGrow: 1 }} color="inherit" variant="h6">{title}</Typography>
+
+          <Button color="secondary" variant="contained">Sign in</Button>
+        </Toolbar>
+      </AppBar>
     );
   }
 }
