27369026164b4706de42d744cef0382cf3481846
Linus Långberg <langberglinus@gmail.com>
2019-01-12 23:34:04 +0100

    Add theming option

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -7,16 +9,24 @@ import Button from '@material-ui/core/Button';
 
 const title = 'React + Material-UI + Firebase';
 
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  }
+});
+
 class App extends Component {
   render() {
     return (
-      <AppBar color="primary" position="static">
-        <Toolbar variant="regular">
-          <Typography style={{ flexGrow: 1 }} color="inherit" variant="h6">{title}</Typography>
+      <MuiThemeProvider theme={theme}>
+        <AppBar color="primary" position="static">
+          <Toolbar variant="regular">
+            <Typography style={{ flexGrow: 1 }} color="inherit" variant="h6">{title}</Typography>
 
-          <Button color="secondary" variant="contained">Sign in</Button>
-        </Toolbar>
-      </AppBar>
+            <Button color="secondary" variant="contained">Sign in</Button>
+          </Toolbar>
+        </AppBar>
+      </MuiThemeProvider>
     );
   }
 }
