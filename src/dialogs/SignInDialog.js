0d2cc51b40a92045a7aaa3c737de513ea0b837d0
Linus Långberg <langberglinus@gmail.com>
2019-01-17 19:46:38 +0100

    Add sign in dialog component

---
+++ src/dialogs/SignInDialog.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+
+import validate from 'validate.js';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const initialState = {
+  emailAddress: '',
+  password: '',
+
+  errors: null
+};
+
+class SignInDialog extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = initialState;
+  }
+
+  signIn = () => {
+    const constraints = {
+      emailAddress: {
+        email: true,
+        presence: true
+      },
+      
+      password: {
+        length: {
+          minimum: 6
+        },
+        presence: true
+      }
+    };
+
+    const { emailAddress, password } = this.state;
+    
+    const errors = validate({ emailAddress, password }, constraints);
+
+    if (errors) {
+      this.setState({ errors });
+    } else {
+      this.setState({
+        errors: null
+      }, () => {
+        this.props.signIn(emailAddress, password);
+      });
+    }
+  };
+
+  handleExited = () => {
+    this.setState(initialState);
+  };
+
+  handleKeyPress = (event) => {
+    const key = event.key;
+
+    if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
+      return;
+    }
+
+    if (key === 'Enter') {
+      this.signIn();
+    }
+  };
+
+  handleEmailAddressChange = (event) => {
+    const emailAddress = event.target.value;
+
+    this.setState({ emailAddress });
+  };
+
+  handlePasswordChange = (event) => {
+    const password = event.target.value;
+
+    this.setState({ password });
+  };
+
+  handleSignInClick = () => {
+    this.signIn();
+  };
+
+  render() {
+    // Properties
+    const { open, isSigningIn } = this.props;
+
+    // Methods
+    const { resetPassword } = this.props;
+
+    // Events
+    const { onClose } = this.props;
+
+    const { emailAddress, password, errors } = this.state;
+
+    return (
+      <Dialog open={open} onClose={onClose} onExited={this.handleExited} onKeyPress={this.handleKeyPress}>
+        <DialogTitle>
+          Sign in to your account
+        </DialogTitle>
+
+        <DialogContent>
+          <DialogContentText>
+            While signed in you can manage your account.
+          </DialogContentText>
+
+          <div>
+            <form>
+              <TextField
+                autoComplete="email"
+                autoFocus
+                error={(errors && errors.emailAddress) ? true : false}
+                fullWidth
+                helperText={(errors && errors.emailAddress) ? errors.emailAddress[0] : ''}
+                margin="normal"
+                onChange={this.handleEmailAddressChange}
+                placeholder="E-mail address"
+                required type="email"
+                value={emailAddress}
+              />
+
+              <TextField
+                autoComplete="password"
+                error={(errors && errors.password) ? true : false}
+                fullWidth margin="normal"
+                helperText={(errors && errors.password) ? errors.password[0] : ''}
+                onChange={this.handlePasswordChange}
+                placeholder="Password"
+                required type="password"
+                value={password}
+              />
+            </form>
+          </div>
+        </DialogContent>
+
+        <DialogActions>
+          <Button color="primary" onClick={onClose}>Cancel</Button>
+          <Button color="primary" onClick={resetPassword}>Reset Password</Button>
+          <Button color="primary" disabled={isSigningIn} onClick={this.handleSignInClick}>Sign in</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default SignInDialog;
