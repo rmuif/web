49857eae5b611505df5a10673519db9f5dbbe722
Linus Långberg <linus.phoqe@gmail.com>
2019-02-08 20:04:42 +0100

    Add EmptyState component

---
+++ src/layout/EmptyState.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+
+const styles = (theme) => ({
+  center: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center'
+  }
+});
+
+class EmptyState extends Component {
+  render() {
+    const { classes, icon, text } = this.props;
+
+    return (
+      <div className={classes.center}>
+        {icon}
+        <Typography variant="h4">{text}</Typography>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(EmptyState);
