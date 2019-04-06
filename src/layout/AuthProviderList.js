f3b62d2a28fb09665409e9edd7af820dca90030d
Linus Långberg <linus.phoqe@gmail.com>
2019-04-06 16:16:38 +0200

    Create AuthProviderList.js

---
+++ src/layout/AuthProviderList.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import firebase from 'firebase/app';
+
+import { withStyles } from '@material-ui/core/styles';
+
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+
+import GoogleIcon from 'mdi-material-ui/Google';
+import FacebookBoxIcon from 'mdi-material-ui/FacebookBox';
+import TwitterIcon from 'mdi-material-ui/Twitter';
+import GitHubCircleIcon from 'mdi-material-ui/GithubCircle';
+
+const styles = (theme) => ({
+  dialogActions: {
+    marginTop: `${theme.spacing.unit * 2.5}px`,
+    marginBottom: `${theme.spacing.unit * 2}px`,
+
+    justifyContent: 'center'
+  },
+
+  google: {
+    backgroundColor: '#4285f4',
+    color: '#ffffff'
+  },
+
+  facebook: {
+    backgroundColor: '#3c5a99',
+    color: '#ffffff'
+  },
+
+  twitter: {
+    backgroundColor: '#1da1f2',
+    color: '#ffffff'
+  },
+
+  gitHub: {
+    backgroundColor: '#24292e',
+    color: '#ffffff'
+  },
+
+  icon: {
+    marginRight: `${theme.spacing.unit / 2}px`
+  }
+});
+
+class AuthProviderList extends Component {
+  render() {
+    // Properties
+    const { classes, isSigningIn } = this.props;
+
+    // Events
+    const { onAuthProviderClick } = this.props;
+
+    return (
+      <DialogActions className={classes.dialogActions}>
+        <Button className={classes.google} disabled={isSigningIn} variant="contained" onClick={() => onAuthProviderClick(new firebase.auth.GoogleAuthProvider())}>
+          <GoogleIcon className={classes.icon} />
+          Google
+        </Button>
+
+        <Button className={classes.facebook} disabled variant="contained" onClick={() => onAuthProviderClick(new firebase.auth.FacebookAuthProvider())}>
+          <FacebookBoxIcon className={classes.icon} />
+          Facebook
+        </Button>
+
+        <Button className={classes.twitter} disabled variant="contained" onClick={() => onAuthProviderClick(new firebase.auth.TwitterAuthProvider())}>
+          <TwitterIcon className={classes.icon} />
+          Twitter
+        </Button>
+
+        <Button className={classes.gitHub} disabled variant="contained" onClick={() => onAuthProviderClick(new firebase.auth.GithubAuthProvider())}>
+          <GitHubCircleIcon className={classes.icon} />
+          GitHub
+        </Button>
+      </DialogActions>
+    );
+  }
+}
+
+AuthProviderList.propTypes = {
+  classes: PropTypes.object.isRequired,
+
+  onAuthProviderClick: PropTypes.func.isRequired
+};
+
+export default withStyles(styles)(AuthProviderList);
