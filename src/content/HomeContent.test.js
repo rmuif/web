69b6ca95c0013ff8b3071fe9c75cbaaf5bbeb94e
Linus Långberg <phoqe@phoqe.dev>
2019-04-18 01:41:55 +0300

    Create HomeContent.test.js

---
+++ src/content/HomeContent.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import HomeContent from './HomeContent';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    (
+      <MemoryRouter>
+        <HomeContent />
+      </MemoryRouter>
+    ),
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
