e31b850cc2fcaf61f44e2b39ab96d3f4eb217be2
Linus Långberg <phoqe@phoqe.dev>
2019-04-18 01:51:37 +0300

    Create AuthProviderList.test.js

---
+++ src/layout/AuthProviderList.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import AuthProviderList from './AuthProviderList';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    (
+      <AuthProviderList />
+    ),
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
