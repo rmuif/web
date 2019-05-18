0922ff7d59efa9c8a5fe1e03abf1dd97bfe65dbb
Linus Långberg <phoqe@phoqe.dev>
2019-05-18 20:47:23 +0200

    Create Profile.test.js

---
+++ src/layout/Profile.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import Profile from './Profile';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    (
+      <Profile
+        user={{}}
+      />
+    ),
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
