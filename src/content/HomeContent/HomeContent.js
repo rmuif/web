98cdca3d8557c15f1d3faa75f1c53f81ad7e24c5
Linus Långberg <phoqe@phoqe.dev>
2019-06-07 19:11:52 +0200

    Move EmptyState into separate folder

--- src/content/HomeContent/HomeContent.js
+++ src/content/HomeContent/HomeContent.js
@@ -13,7 +13,7 @@ import HomeIcon from '@material-ui/icons/Home';
 
 import GitHubCircleIcon from 'mdi-material-ui/GithubCircle';
 
-import EmptyState from '../../layout/EmptyState';
+import EmptyState from '../../layout/EmptyState/EmptyState';
 
 const styles = (theme) => ({
   emptyStateIcon: {
