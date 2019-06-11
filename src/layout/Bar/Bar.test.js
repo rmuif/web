01681ddda09e6d054e63cd8dc77ca73cb23e00cf
Linus Långberg <phoqe@phoqe.dev>
2019-06-11 11:59:14 +0200

    Change tests

--- src/layout/Bar/Bar.test.js
+++ src/layout/Bar/Bar.test.js
@@ -10,7 +10,7 @@ it('renders without crashing', () => {
   ReactDOM.render(
     (
       <Bar
-        name=""
+        title=""
         isPerformingAuthAction
         isSignedIn={false}
         onSettingsClick={() => {}}
