b4402c5a51f8f6fcea7e18e8f1ba1ee7bac8b188
Linus Långberg <phoqe@phoqe.dev>
2019-06-19 14:25:09 +0200

    Add new SimpleDialog

---
+++ src/dialogs/SimpleDialog/SimpleDialog.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+import PropTypes from 'prop-types';
+
+/**
+ * Simple dialogs can display items that are immediately actionable when selected. They don’t have text buttons. As
+ * simple dialogs are interruptive, they should be used sparingly. Alternatively, dropdown menus provide options in a
+ * non-modal, less disruptive way.
+ */
+class SimpleDialog extends Component {
+  render() {
+
+    // Dialog Properties
+    const { dialogProps } = this.props;
+
+    // Custom Properties
+    const {
+      title,
+      content,
+    } = this.props;
+
+    return (
+      <Dialog {...dialogProps}>
+        {title &&
+        <DialogTitle>{title}</DialogTitle>
+        }
+
+        {content}
+      </Dialog>
+    );
+  }
+}
+
+SimpleDialog.propTypes = {
+
+  // Dialog Properties
+  dialogProps: PropTypes.object.isRequired,
+
+  // Custom Properties
+  title: PropTypes.string,
+  content: PropTypes.element.isRequired,
+};
+
+export default SimpleDialog;
---
+++ src/dialogs/SimpleDialog/SimpleDialog.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import SimpleDialog from './SimpleDialog';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <SimpleDialog
+      dialogProps={
+        {
+          open: true,
+
+          onClose: () => {}
+        }
+      }
+
+      content={<div></div>}
+    />,
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
