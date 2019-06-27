446d90f557a0ae864daba87979b2b76a28bebddb
Linus Långberg <phoqe@phoqe.me>
2019-06-27 16:56:03 +0200

    Add profile completion and security rating

--- src/tabs/AccountTab/AccountTab.js
+++ src/tabs/AccountTab/AccountTab.js
@@ -5,6 +5,9 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 import DialogContent from '@material-ui/core/DialogContent';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -51,6 +54,9 @@ const styles = (theme) => ({
 });
 
 const initialState = {
+  profileCompletion: 0,
+  securityRating: 0,
+
   showingField: '',
 
   firstName: '',
@@ -68,6 +74,22 @@ class AccountTab extends Component {
     this.state = initialState;
   }
 
+  calculateProfileCompletion = (fields) => {
+    let profileCompletion = 0;
+
+    fields.forEach((field) => {
+      if (field) {
+        profileCompletion += 100 / fields.length;
+      }
+    });
+
+    return Math.floor(profileCompletion);
+  };
+
+  calculateSecurityRating = () => {
+    return 100;
+  };
+
   showField = (fieldId) => {
     if (!fieldId) {
       return;
@@ -79,7 +101,16 @@ class AccountTab extends Component {
   };
 
   hideFields = () => {
-    this.setState(initialState);
+    this.setState({
+      showingField: '',
+
+      firstName: '',
+      lastName: '',
+      username: '',
+      emailAddress: '',
+
+      errors: null
+    });
   };
 
   changeFirstName = () => {
@@ -223,26 +254,6 @@ class AccountTab extends Component {
     }
   };
 
-  handleKeyPress = (event, fieldId) => {
-    if (!event || !fieldId) {
-      return;
-    }
-
-    if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) {
-      return;
-    }
-
-    const key = event.key;
-
-    if (!key) {
-      return;
-    }
-
-    if (key === 'Enter') {
-      this.changeField(fieldId);
-    }
-  };
-
   handleFirstNameChange = (event) => {
     if (!event) {
       return;
@@ -302,6 +313,9 @@ class AccountTab extends Component {
     } = this.props;
 
     const {
+      profileCompletion,
+      securityRating,
+
       showingField,
 
       firstName,
@@ -314,16 +328,52 @@ class AccountTab extends Component {
 
     return (
       <DialogContent>
-        <Box mb={1} textAlign="center">
-          <Box mb={1}>
-            <Avatar className={classes.avatar} alt="Avatar" src={user.photoURL} />
-          </Box>
+        <Grid alignItems="center" container>
+          <Grid item xs>
+            <Box textAlign="center">
+              <Typography gutterBottom variant="body1">Profile Completion</Typography>
 
-          <Button color="primary" variant="contained">
-            <CloudUploadIcon className={classes.uploadButtonIcon} />
-            Upload
-          </Button>
-        </Box>
+              {profileCompletion === 100 &&
+                <Box clone fontSize={50}>
+                  <CheckCircleIcon color="primary" />
+                </Box>
+              }
+
+              {profileCompletion !== 100 &&
+                <CircularProgress color="secondary" size={50} value={profileCompletion} variant="static" />
+              }
+            </Box>
+          </Grid>
+
+          <Grid item xs>
+            <Box textAlign="center">
+              <Box mb={1}>
+                <Avatar className={classes.avatar} alt="Avatar" src={user.photoURL} />
+              </Box>
+
+              <Button color="primary" variant="contained">
+                <CloudUploadIcon className={classes.uploadButtonIcon} />
+                Upload
+              </Button>
+            </Box>
+          </Grid>
+
+          <Grid item xs>
+            <Box textAlign="center">
+              <Typography gutterBottom variant="body1">Security Rating</Typography>
+
+              {securityRating === 100 &&
+                <Box clone fontSize={50}>
+                  <CheckCircleIcon color="primary" />
+                </Box>
+              }
+
+              {securityRating !== 100 &&
+                <CircularProgress color="secondary" size={50} value={securityRating} variant="static" />
+              }
+            </Box>
+          </Grid>
+        </Grid>
 
         <List disablePadding>
           <ListItem>
@@ -577,6 +627,26 @@ class AccountTab extends Component {
         </List>
       </DialogContent>
     );
+  }
+
+  componentDidMount() {
+    const { user, userData } = this.props;
+
+    const profileCompletion = this.calculateProfileCompletion([
+      user.photoURL,
+      userData.firstName,
+      userData.lastName,
+      userData.username,
+      user.email,
+      user.emailVerified
+    ]);
+
+    const securityRating = this.calculateSecurityRating();
+
+    this.setState({
+      profileCompletion: profileCompletion,
+      securityRating: securityRating
+    });
   }
 }
 
