f465ebfdd9430a2066697645593cc132571276a9
Linus Långberg <phoqe@phoqe.dev>
2019-08-04 18:17:24 +0200

    Use new colors object

--- src/settings.js
+++ src/settings.js
@@ -1,40 +1,12 @@
-/* eslint-disable no-unused-vars */
-
-import red from '@material-ui/core/colors/red';
-import pink from '@material-ui/core/colors/pink';
-import purple from '@material-ui/core/colors/purple';
-import deepPurple from '@material-ui/core/colors/deepPurple';
-import indigo from '@material-ui/core/colors/indigo';
-import blue from '@material-ui/core/colors/blue';
-import lightBlue from '@material-ui/core/colors/lightBlue';
-import cyan from '@material-ui/core/colors/cyan';
-import teal from '@material-ui/core/colors/teal';
-import green from '@material-ui/core/colors/green';
-import lightGreen from '@material-ui/core/colors/lightGreen';
-import lime from '@material-ui/core/colors/lime';
-import yellow from '@material-ui/core/colors/yellow';
-import amber from '@material-ui/core/colors/amber';
-import orange from '@material-ui/core/colors/orange';
-import deepOrange from '@material-ui/core/colors/deepOrange';
-import brown from '@material-ui/core/colors/brown';
-import gray from '@material-ui/core/colors/grey';
-import blueGray from '@material-ui/core/colors/blueGrey';
-
-/* eslint-enable no-unused-vars */
+import colors from './colors';
 
 const settings = {
   title: 'React + Material-UI + Firebase',
 
   theme: {
-    primaryColor: {
-      name: 'blue',
-      import: blue
-    },
-    secondaryColor: {
-      name: 'red',
-      import: red
-    },
-    type: 'light'
+    primaryColor: colors.blue,
+    secondaryColor: colors.red,
+    dark: false
   },
 
   credentials: {
