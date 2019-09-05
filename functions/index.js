a4e2ca51711834dd8226f2c308c5bfb193ea6e79
Linus Långberg <phoqe@phoqe.dev>
2019-09-05 12:46:14 +0200

    Remove `setDefaultAvatar` function

--- functions/index.js
+++ functions/index.js
@@ -5,20 +5,6 @@ admin.initializeApp();
 
 const firestore = admin.firestore();
 
-// exports.setDefaultAvatar = functions.auth.user().onCreate((user) => {
-//   const avatar = user.photoURL;
-//
-//   if (avatar) {
-//     return null;
-//   }
-//
-//   const uid = user.uid;
-//
-//   return admin.auth().updateUser(uid, {
-//     photoURL: `https://api.adorable.io/avatars/285/${uid}.png`
-//   });
-// });
-
 exports.deleteAccount = functions.auth.user().onDelete((user) => {
   const uid = user.uid;
 
