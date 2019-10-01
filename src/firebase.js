8b9c122e0e0af9a38a49010c8eb31861970e97d0
Linus Långberg <phoqe@phoqe.dev>
2019-10-01 12:00:40 +0200

    Initialize analytics

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import * as firebase from 'firebase/app';
 
+import 'firebase/analytics';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
@@ -17,6 +18,7 @@ firebase.initializeApp({
 });
 
 export default firebase;
+export const analytics = firebase.analytics();
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
