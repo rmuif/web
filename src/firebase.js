83cebf3b7da8bb0c1285c9ac685a1789830f3027
Linus Långberg <phoqe@phoqe.dev>
2019-10-01 12:00:32 +0200

    Add `measurementId`

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,8 @@ firebase.initializeApp({
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
 export default firebase;
