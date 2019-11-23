b114b3067a9f45bbc0d4100f493d849e936608b7
Linus Långberg <phoqe@phoqe.dev>
2019-11-23 15:49:21 +0100

    Add AboutDialog component

---
+++ src/components/AboutDialog/AboutDialog.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import AboutDialog from './AboutDialog';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <AboutDialog
+      dialogProps={
+        {
+          open: true,
+
+          onClose: () => {}
+        }
+      }
+    />,
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
