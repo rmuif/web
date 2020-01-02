a2a1689f0315d9d06bf53bdd2729a743d9aa08a9
Linus Långberg <phoqe@phoqe.dev>
2020-01-02 22:33:25 +0100

    Add UserAvatar component

---
+++ src/components/UserAvatar/UserAvatar.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import UserAvatar from './UserAvatar';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(
+    <UserAvatar
+      user={{}}
+    />,
+    div
+  );
+
+  ReactDOM.unmountComponentAtNode(div);
+});
