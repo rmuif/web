1ac4e93a3c00404ae6d5a2d5ee841e01fbd4c748
Linus Långberg <git@phoqe.dev>
2020-02-26 14:35:47 +0100

    optimal-imports

--- src/services/appearance.js
+++ src/services/appearance.js
@@ -2,27 +2,29 @@ import camelCase from "camelcase";
 
 import { createMuiTheme } from "@material-ui/core/styles";
 
-import red from "@material-ui/core/colors/red";
-import pink from "@material-ui/core/colors/pink";
-import purple from "@material-ui/core/colors/purple";
-import deepPurple from "@material-ui/core/colors/deepPurple";
-import indigo from "@material-ui/core/colors/indigo";
-import blue from "@material-ui/core/colors/blue";
-import lightBlue from "@material-ui/core/colors/lightBlue";
-import cyan from "@material-ui/core/colors/cyan";
-import teal from "@material-ui/core/colors/teal";
-import green from "@material-ui/core/colors/green";
-import lightGreen from "@material-ui/core/colors/lightGreen";
-import lime from "@material-ui/core/colors/lime";
-import yellow from "@material-ui/core/colors/yellow";
-import amber from "@material-ui/core/colors/amber";
-import orange from "@material-ui/core/colors/orange";
-import deepOrange from "@material-ui/core/colors/deepOrange";
-import brown from "@material-ui/core/colors/brown";
-import gray from "@material-ui/core/colors/grey";
-import blueGray from "@material-ui/core/colors/blueGrey";
-
 import firebase, { analytics, auth, firestore } from "../firebase";
+
+import {
+  red,
+  pink,
+  purple,
+  deepPurple,
+  indigo,
+  blue,
+  lightBlue,
+  cyan,
+  teal,
+  green,
+  lightGreen,
+  lime,
+  yellow,
+  amber,
+  orange,
+  deepOrange,
+  brown,
+  grey as gray,
+  blueGrey as blueGray
+} from "@material-ui/core/colors";
 
 const colors = {
   red: {
