8c172de07648593963df6f4e4361923f6f1bdc61
Linus Långberg <git@phoqe.dev>
2020-02-26 14:47:29 +0100

    Create config-overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+const { fixBabelImports, override } = require("customize-cra");
+
+module.exports = override(
+  fixBabelImports("@material-ui/core", {
+    libraryDirectory: "esm",
+    camel2DashComponentName: false
+  }),
+
+  fixBabelImports("@material-ui/icons", {
+    libraryDirectory: "esm",
+    camel2DashComponentName: false
+  })
+);
