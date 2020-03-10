7d3c971a65db449cac0f1405df8818f44200bf85
Linus Långberg <git@phoqe.dev>
2020-03-10 17:09:17 +0100

    Add not found illustration

--- template/src/components/NotFoundContent/NotFoundContent.js
+++ template/src/components/NotFoundContent/NotFoundContent.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 
-import { FindInPage as FindInPageIcon } from "@material-ui/icons";
-
 import EmptyState from "../EmptyState";
+
+import { ReactComponent as PageNotFoundIllustration } from "../../illustrations/page-not-found.svg";
 
 class NotFoundContent extends Component {
   render() {
     return (
       <EmptyState
-        icon={<FindInPageIcon />}
-        title="Content not found"
+        image={<PageNotFoundIllustration />}
+        title="Page not found"
         description="The requested URL was not found on this server"
       />
     );
