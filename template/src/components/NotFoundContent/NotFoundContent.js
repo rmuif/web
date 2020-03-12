a125fc697ed49dc3c34e9151552b4f8973005777
Linus Långberg <git@phoqe.dev>
2020-03-12 21:18:54 +0100

    Add Home button for 404

--- template/src/components/NotFoundContent/NotFoundContent.js
+++ template/src/components/NotFoundContent/NotFoundContent.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+import { Link } from "react-router-dom";
+
+import { Fab, Box } from "@material-ui/core";
+
+import { Home as HomeIcon } from "@material-ui/icons";
+
 import EmptyState from "../EmptyState";
 
 import { ReactComponent as NotFoundIllustration } from "../../illustrations/not-found.svg";
@@ -11,6 +17,14 @@ class NotFoundContent extends Component {
         image={<NotFoundIllustration />}
         title="Page not found"
         description="The page doesn’t exist"
+        button={
+          <Fab variant="extended" color="primary" component={Link} to="/">
+            <Box clone mr={1}>
+              <HomeIcon />
+            </Box>
+            Home
+          </Fab>
+        }
       />
     );
   }
