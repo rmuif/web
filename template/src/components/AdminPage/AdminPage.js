993535c35ad3ee4a12fa674bd0d9f45432713245
Linus Långberg <git@phoqe.dev>
2020-03-15 12:38:41 +0100

    Rename AdminContent to AdminPage

--- template/src/components/AdminPage/AdminPage.js
+++ template/src/components/AdminPage/AdminPage.js
@@ -2,10 +2,10 @@ import React, { Component } from "react";
 
 import EmptyState from "../EmptyState";
 
-class AdminContent extends Component {
+class AdminPage extends Component {
   render() {
     return <EmptyState title="Admin" />;
   }
 }
 
-export default AdminContent;
+export default AdminPage;
