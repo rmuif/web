85eff12c8b7cb4833dac0ffdf2ca7473ec5ab8de
zaabalonso <zaabalonso@gmail.com>
2020-04-19 22:33:46 -0700

    Fixing import failures issue https://github.com/rmuif/web/issues/565

--- template/src/authProviders.js
+++ template/src/authProviders.js
@@ -1,56 +1,55 @@
-import React from "react";
+import React from 'react';
 
-import { Apple as AppleIcon } from "mdi-material-ui";
-import { FacebookBox as FacebookBoxIcon } from "mdi-material-ui";
-import { GithubCircle as GitHubCircleIcon } from "mdi-material-ui";
-import { Google as GoogleIcon } from "mdi-material-ui";
-import { Microsoft as MicrosoftIcon } from "mdi-material-ui";
-import { Twitter as TwitterIcon } from "mdi-material-ui";
-import { Yahoo as YahooIcon } from "mdi-material-ui";
+import {Apple as AppleIcon} from 'mdi-material-ui';
+import {FacebookBox, Github} from 'mdi-material-ui';
+import {Google as GoogleIcon} from 'mdi-material-ui';
+import {Microsoft as MicrosoftIcon} from 'mdi-material-ui';
+import {Twitter as TwitterIcon} from 'mdi-material-ui';
+import {Yahoo as YahooIcon} from 'mdi-material-ui';
 
 const authProviders = [
   {
-    providerId: "apple.com",
-    color: "#000000",
+    providerId: 'apple.com',
+    color: '#000000',
     icon: <AppleIcon />,
-    name: "Apple"
+    name: 'Apple',
   },
   {
-    providerId: "facebook.com",
-    color: "#3c5a99",
-    icon: <FacebookBoxIcon />,
-    name: "Facebook"
+    providerId: 'facebook.com',
+    color: '#3c5a99',
+    icon: <Facebook />,
+    name: 'Facebook',
   },
   {
-    providerId: "github.com",
-    color: "#24292e",
-    icon: <GitHubCircleIcon />,
-    name: "GitHub"
+    providerId: 'github.com',
+    color: '#24292e',
+    icon: <Github />,
+    name: 'GitHub',
   },
   {
-    providerId: "google.com",
-    color: "#4285f4",
+    providerId: 'google.com',
+    color: '#4285f4',
     icon: <GoogleIcon />,
-    name: "Google"
+    name: 'Google',
   },
   {
-    providerId: "microsoft.com",
-    color: "#f65314",
+    providerId: 'microsoft.com',
+    color: '#f65314',
     icon: <MicrosoftIcon />,
-    name: "Microsoft"
+    name: 'Microsoft',
   },
   {
-    providerId: "twitter.com",
-    color: "#1da1f2",
+    providerId: 'twitter.com',
+    color: '#1da1f2',
     icon: <TwitterIcon />,
-    name: "Twitter"
+    name: 'Twitter',
   },
   {
-    providerId: "yahoo.com",
-    color: "#410093",
+    providerId: 'yahoo.com',
+    color: '#410093',
     icon: <YahooIcon />,
-    name: "Yahoo"
-  }
+    name: 'Yahoo',
+  },
 ];
 
 export default authProviders;
