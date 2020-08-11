8fe03aca32499fa4079dc65e1e21ced7b5866281
Linus Långberg <git@phoqe.dev>
2020-08-11 10:57:07 +0200

    Improve no profile view

--- template/src/components/UserCard/UserCard.js
+++ template/src/components/UserCard/UserCard.js
@@ -7,23 +7,12 @@ import { Card, CardHeader } from "@material-ui/core";
 function UserCard(props) {
   const user = props.user;
 
-  const hasProfile = user.firstName && user.lastName && user.username;
-
   return (
     <Card>
-      {hasProfile && (
-        <CardHeader
-          title={`${user.firstName} ${user.lastName}`}
-          subheader={user.username}
-        />
-      )}
-
-      {!hasProfile && (
-        <CardHeader
-          title="No profile"
-          subheader="You can setup your profile in Settings."
-        />
-      )}
+      <CardHeader
+        title={`${user.firstName} ${user.lastName}`}
+        subheader={user.username}
+      />
     </Card>
   );
 }
