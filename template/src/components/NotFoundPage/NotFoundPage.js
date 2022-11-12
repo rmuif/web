80e7ed89f7a6151ba0fe79372cb713df6b3d33d0
name <your mail>
2022-11-11 19:32:43 -0700

    class-to-hook-notfound-page

--- template/src/components/NotFoundPage/NotFoundPage.js
+++ template/src/components/NotFoundPage/NotFoundPage.js
@@ -1,33 +1,26 @@
-import React, { Component } from "react";
-
+import React from "react";
 import { Link } from "react-router-dom";
-
 import { Fab, Box } from "@material-ui/core";
-
 import { Home as HomeIcon } from "@material-ui/icons";
-
 import EmptyState from "../EmptyState";
-
 import { ReactComponent as NotFoundIllustration } from "../../illustrations/not-found.svg";
 
-class NotFoundPage extends Component {
-  render() {
-    return (
-      <EmptyState
-        image={<NotFoundIllustration />}
-        title="Page doesn’t exist."
-        description="The page you’re trying to access doesn’t exist."
-        button={
-          <Fab variant="extended" color="primary" component={Link} to="/">
-            <Box clone mr={1}>
-              <HomeIcon />
-            </Box>
-            Home
-          </Fab>
-        }
+function NotFoundPage() {
+  return (
+    <EmptyState
+      image={<NotFoundIllustration />}
+      title="Page doesn't exist."
+      description="The page you're trying to access doesn't exist."
+      button={
+        <Fab variant="extended" color="primary" component={Link} to="/">
+          <Box clone mr={1}>
+            <HomeIcon /> 
+          </Box>
+          Home
+        </Fab> 
+      }
       />
-    );
-  }
+  )
 }
 
-export default NotFoundPage;
+export default NotFoundPage; 
